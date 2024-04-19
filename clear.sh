6e40dc15e44ba9bcac28fbf4806668a633c30077
Wallblue <nrb.busquet@gmail.com>
2024-04-20 00:52:21 +0100

    Add a user and groups cleaning script

---
+++ clear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+deluser MDupont
+deluser MMartin
+deluser MDubois
+deluser PDubois
+deluser EPetit
+deluser blue1
+delgroup GroupeA
+delgroup GroupeB
+delgroup GroupeC
+delgroup GroupeD
